a663bfc775591173bde47c8d5435e2cc5ef8ccb0
anoj <anojbsunk500@gmail.com>
2025-12-10 11:11:17 +0545

    changing photos

--- src/app/testing/component/Twelve.jsx
+++ src/app/testing/component/Twelve.jsx
@@ -7,19 +7,19 @@ export default function Twelve(){
                 
                 <div className='ml-5'>
                     <div className='border-t-2 mt-5'>
-                    <p className='text-[22px] mt-5 mb-5 flex justify-between'>How do i register a domain in Nepal? <ChevronDown  /></p>
+                    <p className='text-[22px] mt-10 flex justify-between'>How do i register a domain in Nepal? <ChevronDown  /></p>
+                </div>
+                <div className='border-t-2 mt-10 mb-10'>
+                    <p className='text-[22px] mt-10 flex justify-between'>What happens if I fail to renew my domain? <ChevronDown/></p>
+                </div>
+                <div className='border-t-2 mt-10 mb-10'>
+                    <p className='text-[22px] mt-10 flex justify-between'>Are there any fees for transferring a domain to Nest Nepal? <ChevronDown  /></p>
+                </div>
+                <div className='border-t-2 mt-10 mb-10'>
+                    <p className='text-[22px] mt-10 flex justify-between'>Can Nest Nepal help in domain registration and DNS changes? <ChevronDown  /></p>
                 </div>
                 <div className='border-t-2 mt-5'>
-                    <p className='text-[22px] mt-5 flex justify-between'>What happens if I fail to renew my domain? <ChevronDown/></p>
-                </div>
-                <div className='border-t-2 mt-5'>
-                    <p className='text-[22px] mt-5 flex justify-between'>Are there any fees for transferring a domain to Nest Nepal? <ChevronDown  /></p>
-                </div>
-                <div className='border-t-2 mt-5'>
-                    <p className='text-[22px] mt-5 flex justify-between'>Can Nest Nepal help in domain registration and DNS changes? <ChevronDown  /></p>
-                </div>
-                <div className='border-t-2 mt-5'>
-                    <p className='text-[22px] mt-5 flex justify-between'>Do you support international domain names? <ChevronDown/></p>
+                    <p className='text-[22px] mt-10 flex justify-between'>Do you support international domain names? <ChevronDown/></p>
                 </div>
                 </div>
                 
